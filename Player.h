be99414cd5e2d089a8ea2cf766a34b3dbf13afea
nissaputrid <44718959+nissaputrid@users.noreply.github.com>
2019-03-18 18:25:01 +0700

    Add files via upload

--- Player.h
+++ Player.h
@@ -1,34 +1,59 @@
-#include "Object.h"
-
 #ifndef PLAYER_H
 #define PLAYER_H
-class Player : public Object{
+#include "Produk.h"
+#include "Object.h"
+
+class Player: public Object{
 	private:
-		int wadahAir;
-		int kapasitas;
-		Produk tas*;
+		int WadahAir;
+		Produk Tas[25];
+		
 	public:
-		void Talk(){
-			x = player.GetX();
-			y = player.GetY();
-			if (cell[x][y].kategori == "Barn") {
-				if (barn[x][y].isOcupied == true) {
-					//moo2
-				}
-			}
-			else if (cell[x][y].kategori == "Coop") {
-				if (coop[x][y].isOcupied == true) {
-					//petok2
-				}
-			}
-			else if (cell[x][y].kategori == "Grassland") {
-				if (grass[x][y].isOcupied == true) {
-					//petok2
-				}
-			}
-			else {
-				//no2
-			}
-		}
+		// ctor
+		Player();
+
+		//cctor
+		Player(const Player& P);
+		
+		//dtor
+		~Player();
+		
+		//assignment
+		Player& operator=(const Player& P);
+		
+		//getter
+		int getWadahAir();
+		int getElTas(int indeks);
+		int getNeffTas();
+		
+		//setter
+		void setWadahAir(int _WadahAir);
+		void setElTas(int indeks, int element);
+		
+		bool isTasFull();
+		bool isTasEmpty();
+		bool isElement(int element);
+		bool isWadahAirFull();
+		bool isWadahAirEmpty();
+		
+		void addProduk(Produk P);
+		void removeProduk(Produk P);
+		int findProduk(Produk P);
+		
+		// Methods
+		void Talk();
+		void Interact();
+		void Kill();
+		void Grow();
+		void Mix();
+
+		//Move
+
+		void Up();
+		void Down();
+		void Left();
+		void Right();
+		
 };
+
 #endif
